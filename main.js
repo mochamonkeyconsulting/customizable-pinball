9a56303cef33eda63063c1f0cb495e0e7a411d99
Josh Mason <jtomason@hotmail.com>
2025-02-09 17:14:04 -0500

    Switched to constraints

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 var Example = Example || {};
 
 
+// other game stuff
+const EVENT_TRIGGERS = [
+    "BALL_LAUNCH",
+    "BALL_LOST",
+    "GAME_START",
+    "MULTIPLIER_ACTIVATED"
+]
+
+const ASSETS = {
+    backgroundMusic: {},
+    background: {},
+    ball: {shuffleTrigger: "BALL_LOST"},
+    
+}
+
+let POINTS = 0
+let GAME_STATES = {
+    TOP_BALL_BOUNCERS: {
+        left: false,
+        middle: false,
+        right: false
+    }
+}
+
+
 Example.manipulation = function() {
     var Engine = Matter.Engine,
         Render = Matter.Render,
@@ -43,7 +68,7 @@ Example.manipulation = function() {
         ballyBoi = Bodies.circle(400, 100, 20, { friction: 0, render: { fillStyle: '#060a19' } });
 
 
-    Composite.add(world, [bodyLeftBase, bodyRightBase, ballyBoi]);
+    Composite.add(world, [ ballyBoi]); // bodyLeftBase, bodyRightBase
 
     Composite.add(world, [
         // walls
@@ -52,21 +77,24 @@ Example.manipulation = function() {
         Bodies.rectangle(675, 300, 50, 800, { isStatic: true }), // right left
         Bodies.rectangle(800, 300, 50, 1200, { isStatic: true }), //right right
         Bodies.rectangle(0, 300, 50, 1200, { isStatic: true }), //left
-        Bodies.rectangle(165, 300, 50, 1200, { isStatic: true }), //left
+        Bodies.rectangle(50, 100, 50, 800, {angle: -.2,  isStatic: true }), //left
         Bodies.rectangle(725, -230, 50, 200, { angle: -.9, isStatic: true }) // bumper thing at top
     ]);
 
 
+
+    
     // LEFT FLAPPER
     var groupLeft = Body.nextGroup(true),
         length = 150,
         width = 25;
 
-    var leftFlapper = Composites.stack(200, 500, 1, 1, -20, 0, function(x, y) {
+    var leftFlapper = Composites.stack(150, 500, 1, 1, -20, 0, function(x, y) {
         return Bodies.rectangle(x, y, length, width, { 
             collisionFilter: { group: groupLeft },
             frictionAir: 0,
             chamfer: 5,
+            //restitution: .9, 
             render: {
                 fillStyle: 'blue',
                 lineWidth: 1
@@ -77,21 +105,30 @@ Example.manipulation = function() {
         bodyB: leftFlapper.bodies[0],
         pointB: { x: -length * 0.42, y: 0 },
         pointA: { x: leftFlapper.bodies[0].position.x - length * 0.42, y: leftFlapper.bodies[0].position.y },
-        stiffness: 0.9,
+        stiffness: 1,
         length: 0,
         render: {
             strokeStyle: '#4a485b'
         }
     }));
+    let leftConstraint = Constraint.create({ 
+        pointA: { x: leftFlapper.bodies[0].position.x + 0, y: leftFlapper.bodies[0].position.y - 100 },
+        bodyB: leftFlapper.bodies[0],
+        pointB: { x:  50, y: 0},
+        stiffness: .9,
+        length: 160,
+        damping: 1,
+    })
+    Composite.add(leftFlapper, leftConstraint);
     Composite.add(world, leftFlapper);
 
 
     // RIGHT FLAPPER
-        var groupRight = Body.nextGroup(true),
+    var groupRight = Body.nextGroup(true),
         length = 150,
         width = 25;
 
-    var rightFlapper = Composites.stack(575, 500, 1, 1, -20, 0, function(x, y) {
+    var rightFlapper = Composites.stack(450, 500, 1, 1, -20, 0, function(x, y) {
         return Bodies.rectangle(x, y, length, width, { 
             collisionFilter: { group: groupRight },
             frictionAir: 0,
@@ -106,12 +143,21 @@ Example.manipulation = function() {
         bodyB: rightFlapper.bodies[0],
         pointB: { x: -length * 0.42, y: 0 },
         pointA: { x: rightFlapper.bodies[0].position.x - length * 0.42, y: rightFlapper.bodies[0].position.y },
-        stiffness: 0.9,
+        stiffness: 1,
         length: 0,
         render: {
             strokeStyle: '#4a485b'
         }
     }));
+    let rightConstraint = Constraint.create({ 
+        pointA: { x: rightFlapper.bodies[0].position.x -100, y: rightFlapper.bodies[0].position.y - 100 },
+        bodyB: rightFlapper.bodies[0],
+        pointB: { x:  50, y: 0},
+        stiffness: .9,
+        length: 160,
+        damping: 1,
+    })
+    Composite.add(rightFlapper, rightConstraint);
     Composite.add(world, rightFlapper);
 
 
@@ -170,21 +216,25 @@ Example.manipulation = function() {
         }
         
         if(upPressed==true){
-            Body.setPosition(ballyBoi, { x: 325, y: -100 }, true);
+            Body.setPosition(ballyBoi, { x: 225, y: -100 }, true);
             
             // Body.setVelocity(ballyBoi, { x: Math.floor(Math.random() * 10)-5, y: Math.floor(Math.random() * 10)-5 });
             Body.setVelocity(ballyBoi, { x: 0, y: 0 });
         }
 
         if(leftPressed==true){
-            
-            Body.setVelocity(Composite.allBodies(leftFlapper)[0], { x: 10, y: -40 });
+            leftConstraint.length=0
+            leftConstraint.stiffness=.01
+        }else{
+            leftConstraint.length=150
+            leftConstraint.stiffness=.1
         }
 
         
         if(rightPressed==true){
-            
-            Body.setVelocity(Composite.allBodies(rightFlapper)[0], { x: -10, y: -40 });
+            rightConstraint.length=0
+        }else{
+            rightConstraint.length=150
         }
 
 
@@ -216,6 +266,45 @@ Example.manipulation = function() {
         max: { x: 800, y: 600 }
     });
 
+
+
+    // var canvas = document.createElement('canvas'),
+    // context = canvas.getContext('2d');
+    
+    // canvas.width = 800;
+    // canvas.height = 600;
+    
+    // document.body.appendChild(canvas);
+    
+    // (function render() {
+    // var bodies = Composite.allBodies(engine.world);
+    
+    // window.requestAnimationFrame(render);
+    
+    // context.fillStyle = '#fff';
+    // context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    // context.beginPath();
+    
+    // for (var i = 0; i < bodies.length; i += 1) {
+    //     var vertices = bodies[i].vertices;
+    
+    //     context.moveTo(vertices[0].x, vertices[0].y);
+    
+    //     for (var j = 1; j < vertices.length; j += 1) {
+    //         context.lineTo(vertices[j].x, vertices[j].y);
+    //     }
+    
+    //     context.lineTo(vertices[0].x, vertices[0].y);
+    // }
+    
+    // context.lineWidth = 1;
+    // context.strokeStyle = '#999';
+    // context.stroke();
+    // })();
+    
+
+
     // context for MatterTools.Demo
     return {
         engine: engine,
@@ -227,6 +316,7 @@ Example.manipulation = function() {
             Matter.Runner.stop(runner);
         }
     };
+    
 };
 
 Example.manipulation.title = 'Manipulation';
